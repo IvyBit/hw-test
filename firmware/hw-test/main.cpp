4cf20da9cb0180e5a7805eae2ad76f0c8611d433
Alexander Hammer <alexander@ivybit.de>
2022-03-27 10:30:02 +0200

    firmware

---
+++ firmware/hw-test/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * hw-test.cpp
+ *
+ * Created: 27/03/2022 10:20:17
+ * Author : Alex
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
